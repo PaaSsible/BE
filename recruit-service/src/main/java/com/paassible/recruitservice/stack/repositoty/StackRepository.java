8b6ab73743b105eb566e27b6347787b926ec58b1
gyuri127 <gyurih127@gmail.com>
2025-08-30 16:39:28 +0900

    :sparkles: feat: 기술 스택 조회 구현

--- recruit-service/src/main/java/com/paassible/recruitservice/stack/repositoty/StackRepository.java
+++ recruit-service/src/main/java/com/paassible/recruitservice/stack/repositoty/StackRepository.java
@@ -3,6 +3,4 @@ package com.paassible.recruitservice.stack.repositoty;
 import com.paassible.recruitservice.stack.entity.Stack;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface StackRepository extends JpaRepository<Stack, Long> {}
