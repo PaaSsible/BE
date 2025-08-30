eb99475ae80481a70964e45bbecf20ba8f7354df
gyuri127 <gyurih127@gmail.com>
2025-08-30 16:39:19 +0900

    :sparkles: feat: position 조회 구현

--- recruit-service/src/main/java/com/paassible/recruitservice/position/controller/PositionController.java
+++ recruit-service/src/main/java/com/paassible/recruitservice/position/controller/PositionController.java
@@ -1,5 +1,7 @@
 package com.paassible.recruitservice.position.controller;
 
+import com.paassible.common.response.ApiResponse;
+import com.paassible.common.response.SuccessCode;
 import com.paassible.recruitservice.position.dto.PositionResponse;
 import com.paassible.recruitservice.position.service.PositionService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,8 +20,11 @@ public class PositionController {
     private final PositionService positionService;
 
     @Operation(summary = "포지션 조회", description = "")
-    @GetMapping("/recruit/position")
-    public List<PositionResponse> getAllPositions() {
-        return positionService.getAllPositions();
+    @GetMapping("/recruit/positions")
+    public ApiResponse<List<PositionResponse>> getAllPositions() {
+        List<PositionResponse> response =  positionService.getAllPositions();
+        return ApiResponse.success(SuccessCode.OK, response);
     }
+
+
 }
