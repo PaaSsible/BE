7a42aa44da0b5e4afc323fe48cc90298dad90526
gyuri127 <gyurih127@gmail.com>
2025-08-30 16:27:26 +0900

    :sparkles: feat: position 조회 구현

---
+++ recruit-service/src/main/java/com/paassible/recruitservice/position/controller/PositionController.java
@@ -0,0 +1,25 @@
+package com.paassible.recruitservice.position.controller;
+
+import com.paassible.recruitservice.position.dto.PositionResponse;
+import com.paassible.recruitservice.position.service.PositionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "포지션 API", description = "포지션 조회")
+public class PositionController {
+
+    private final PositionService positionService;
+
+    @Operation(summary = "포지션 조회", description = "")
+    @GetMapping("/recruit/position")
+    public List<PositionResponse> getAllPositions() {
+        return positionService.getAllPositions();
+    }
+}
--- recruit-service/src/main/java/com/paassible/recruitservice/position/repositoty/PositionRepository.java
+++ recruit-service/src/main/java/com/paassible/recruitservice/position/repositoty/PositionRepository.java
@@ -3,6 +3,4 @@ package com.paassible.recruitservice.position.repositoty;
 import com.paassible.recruitservice.position.entity.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
 public interface PositionRepository extends JpaRepository<Position, Long> {}
