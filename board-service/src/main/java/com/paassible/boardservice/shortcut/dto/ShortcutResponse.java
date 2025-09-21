2679b97626d23d9f459947553a2a0ebc04aa9441
codmsrose <97442570+codmsrose@users.noreply.github.com>
2025-09-21 22:24:32 +0900

    feat: shortcut 응답 boardId 제거

--- board-service/src/main/java/com/paassible/boardservice/shortcut/dto/ShortcutResponse.java
+++ board-service/src/main/java/com/paassible/boardservice/shortcut/dto/ShortcutResponse.java
@@ -8,14 +8,12 @@ import lombok.Getter;
 @Builder
 public class ShortcutResponse {
     private Long id;
-    private Long boardId;
     private String name;
     private String url;
 
     public static ShortcutResponse from(Shortcut shortcut) {
         return ShortcutResponse.builder()
                 .id(shortcut.getId())
-                .boardId(shortcut.getBoardId())
                 .name(shortcut.getName())
                 .url(shortcut.getUrl())
                 .build();
