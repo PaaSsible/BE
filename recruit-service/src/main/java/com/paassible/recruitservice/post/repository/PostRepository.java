0cd70b5f4dfbbd5f2f370af2c98f3592c6ea2ff6
gyuri127 <gyurih127@gmail.com>
2025-09-29 14:17:07 +0900

    feat: 마감일 지나면 게시글 close처리 Ref : #57

--- recruit-service/src/main/java/com/paassible/recruitservice/post/repository/PostRepository.java
+++ recruit-service/src/main/java/com/paassible/recruitservice/post/repository/PostRepository.java
@@ -4,10 +4,15 @@ import com.paassible.recruitservice.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 
     Page<Post> findByWriterId(Long writerId, Pageable pageable);
 
+    List<Post> findByDeadlineBeforeAndClosed(LocalDate now, boolean closed);
 
 }
 
