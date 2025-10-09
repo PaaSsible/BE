435c3e661f0cedd50f81ca815f9c0b808564dec7
codmsrose <97442570+codmsrose@users.noreply.github.com>
2025-10-09 21:10:17 +0900

    feat: 포트폴리오 엔티티 생성

---
+++ user-service/src/main/java/com/paassible/userservice/user/repository/PortfolioRepository.java
@@ -0,0 +1,13 @@
+package com.paassible.userservice.user.repository;
+
+import com.paassible.userservice.user.entity.Portfolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+
+    List<Portfolio> findByUserId(Long userId);
+}
