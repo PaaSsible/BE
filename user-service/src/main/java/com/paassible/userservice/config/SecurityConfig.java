e135c61988dbfd10f305710da60666db77fc9774
gyuri127 <gyurih127@gmail.com>
2025-10-19 01:14:46 +0900

    cors 설정 Ref : #103

--- user-service/src/main/java/com/paassible/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/paassible/userservice/config/SecurityConfig.java
@@ -14,6 +14,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -55,11 +60,27 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
 
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // JWT 필터 등록
                 .addFilterBefore(
                         new JwtAuthenticationFilter(jwtUtil),
                         UsernamePasswordAuthenticationFilter.class)
 
                 .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173"    // 프론트 로컬 개발용
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
