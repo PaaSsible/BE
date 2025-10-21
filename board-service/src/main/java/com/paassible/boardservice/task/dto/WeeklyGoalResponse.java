df4c13b7e0090184b256552a8f497e44db6da194
codmsrose <97442570+codmsrose@users.noreply.github.com>
2025-10-21 15:19:46 +0900

    feat: 주간 업무 달성률 응답 dto

---
+++ board-service/src/main/java/com/paassible/boardservice/task/dto/WeeklyGoalResponse.java
@@ -0,0 +1,10 @@
+package com.paassible.boardservice.task.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class WeeklyGoalResponse {
+    private double weeklyGoalRate;
+}
