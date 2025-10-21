4d9ca4c06f65545ff3bb18aae715cd28cd03ab01
gyuri127 <gyurih127@gmail.com>
2025-10-21 14:48:39 +0900

    feat: cors설정 추가 Ref : #123

--- meet-service/src/main/java/com/paassible/meetservice/config/SecurityConfig.java
+++ meet-service/src/main/java/com/paassible/meetservice/config/SecurityConfig.java
@@ -44,11 +44,27 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
 
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterBefore(
                         new JwtAuthenticationFilter(jwtUtil),
                         UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173"    // 프론트 로컬 개발용
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
