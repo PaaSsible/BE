8e14f7f5fb5c35ffebd12f126874055ad5ae5b0c
gyuri127 <gyurih127@gmail.com>
2025-10-30 12:19:28 +0900

    feat: 회의 알림 기능 구현 Ref : #225

--- meet-service/src/main/java/com/paassible/meetservice/notification/MeetNotificationPublisher.java
+++ meet-service/src/main/java/com/paassible/meetservice/notification/MeetNotificationPublisher.java
@@ -9,31 +9,16 @@ public class MeetNotificationPublisher {
 
     private final NotificationPublisher publisher;
 
-    public void startMeet(Long memberId, String applicantName, String title) {
+    public void notifyMeetStarted(Long memberId, String boardName) {
         publisher.sendNotification(
                 memberId,
                 "MEET",
-                "새로운 지원이 도착했어요!",
-                applicantName + "님이 [" + title + "]에 지원서를 제출했어요.\n함께할 팀원을 검토해 보세요"
+                "회의가 시작됐어요! 지금 바로 참여하세요.",
+                "["+boardName + "] 팀의 온라인 회의에 초대 요청을 받았습니다.\n팀원들과의 논의를 놓치지 않도록 지금 입장해 보세요."
         );
     }
 
-    public void sendAccepted(Long applicantId, String title) {
-        publisher.sendNotification(
-                applicantId,
-                "RECRUIT",
-                "지원하신 프로젝트의 결과가 도착했어요.",
-                "[" + title + "] 팀에서 함께하고 싶다는 소식을 전했어요!\n팀 페이지에서 포지션을 선택하고 작업을 시작해 보세요."
-        );
-    }
 
-    public void sendRejected(Long applicantId, String title) {
-        publisher.sendNotification(
-                applicantId,
-                "RECRUIT",
-                "지원하신 프로젝트의 결과가 도착했어요.",
-                "아쉽지만 ["+ title +"] 팀과는 이번에 함께하지 못하게 되었어요.\n팀의 결정 사유를 확인해 주세요."
-        );
-    }
+
 }
 
