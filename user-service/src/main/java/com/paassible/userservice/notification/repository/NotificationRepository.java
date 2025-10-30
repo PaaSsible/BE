5f0b6edb339e294953dafb33ac516e6789de75a6
codmsrose <97442570+codmsrose@users.noreply.github.com>
2025-10-30 16:28:20 +0900

    feat: 알림 조회

--- user-service/src/main/java/com/paassible/userservice/notification/repository/NotificationRepository.java
+++ user-service/src/main/java/com/paassible/userservice/notification/repository/NotificationRepository.java
@@ -1,11 +1,13 @@
 package com.paassible.userservice.notification.repository;
 
-import com.paassible.userservice.notification.dto.NotificationResponse;
 import com.paassible.userservice.notification.entity.Notification;
+import com.paassible.userservice.notification.entity.enums.NoticeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByUserIdOrderByCreatedAtDesc(Long userId);
+
+    List<Notification> findByUserIdAndTypeOrderByCreatedAtDesc(Long userId, NoticeType type);
 }
