9ba0c36af9f6d1a764723a1ec4855ca92cc11849
codmsrose <97442570+codmsrose@users.noreply.github.com>
2026-02-03 12:12:04 +0900

    feat: securityconfig 수정

--- user-service/src/main/java/com/paassible/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/paassible/userservice/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
+        config.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
                 "https://paassible.netlify.app"
         ));
